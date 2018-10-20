a6fe2f8951fe3267cbb4cb5d5fb4403f991e57fc
Ravi Kalia <ravkalia@gmail.com>
2018-10-20 17:08:06 -0500

    setup elements

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,11 @@
+// get elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+// write out functions
+
+// hook up eventlisteners between functions and elements
