c61fe6559227f5092f87940970f98724ce342e9c
Ravi Kalia <ravkalia@gmail.com>
2018-10-20 19:22:54 -0500

    setting up hooks and functions

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -7,5 +7,17 @@ const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 // write out functions
+const togglePlay = () => video[video.paused ? 'play' : 'pause']();
+
+const updateButton = () => toggle.textContent = video.paused ? '►' : '❚ ❚';
+
+const skip = function () {
+  video.currentTime = this.dataset.skip;
+};
 
 // hook up eventlisteners between functions and elements
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
+[...skipButtons].map(btn => btn.addEventListener('click', skip));
