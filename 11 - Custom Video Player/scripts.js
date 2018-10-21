1a0adc192a744d7662fad7bd26ba5ed701ff03b1
Ravi Kalia <ravkalia@gmail.com>
2018-10-20 20:21:10 -0500

    All Done

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -19,9 +19,13 @@ const handleRangeUpdate = function (params) {
   video[this.name] = this.value;
 };
 
-const handleProgress = function (params) {
-  
+const handleProgress = () => {
+  progressBar
+    .style.flexBasis = `${(video.currentTime / video.duration) * 100}%`;
 };
+
+const setVideoTime = e => video
+  .currentTime = (e.offsetX / progress.offsetWidth) * video.duration;
 
 // hook up eventlisteners between functions and elements
 video.addEventListener('click', togglePlay);
@@ -29,6 +33,12 @@ video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
 [...skipButtons].map(btn => btn.addEventListener('click', skip));
+video.addEventListener('timeupdate', handleProgress);
 
+let mousedown = false;
 [...ranges].map(range => range.addEventListener('click', handleRangeUpdate));
 [...ranges].map(range => range.addEventListener('mousemove', handleRangeUpdate));
+progress.addEventListener('click', setVideoTime);
+progress.addEventListener('mousemove', (e) => mousedown && setVideoTime(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
